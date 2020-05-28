8053ffd937e91c9703db3728245dd77a3637e35b
dwbenjamin <novelluser2002@yahoo.com>
2020-05-28 15:56:33 -0600

    Automatic changes from rubocop

--- test/main_module_test.rb
+++ test/main_module_test.rb
@@ -1,33 +1,30 @@
-#Copying and distribution of this file, with or without modification,
-#are permitted in any medium without royalty provided the copyright
-#notice and this notice are preserved.  This file is offered as-is,
-#without any warranty.
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
 
 require_relative 'spec_helper'
 require 'y2helloworld/main_module'
 
 describe Y2HelloWorld::MainDialog do
-    before(:all) do
-      Yast.ui_component = 'ncurses'
-    end
+  before(:all) do
+    Yast.ui_component = 'ncurses'
+  end
 
-    def mock_dialog data
-      data[:input] ||= :hello
-      ui = double("Yast::UI")
-      stub_const("Yast::UI", ui)
+  def mock_dialog(data)
+    data[:input] ||= :hello
+    ui = double('Yast::UI')
+    stub_const('Yast::UI', ui)
 
-      expect(ui).to receive(:OpenDialog).and_return(true)
+    expect(ui).to receive(:OpenDialog).and_return(true)
 
-      expect(ui).to receive(:CloseDialog).and_return(true)
+    expect(ui).to receive(:CloseDialog).and_return(true)
 
-      expect(ui).to receive(:UserInput).and_return(*data[:input])
+    expect(ui).to receive(:UserInput).and_return(*data[:input])
+  end
 
-    end
-
-    it "return nil if user closes window" do
-      mock_dialog :input => :cancel
-      expect(Y2HelloWorld::MainDialog.new.run).to eq nil
-    end
-
-
+  it 'return nil if user closes window' do
+    mock_dialog input: :cancel
+    expect(Y2HelloWorld::MainDialog.new.run).to eq nil
+  end
 end
